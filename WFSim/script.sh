ce28424d37f37bd04488f3017ed4d15e2d589ab7
suhashidesilva <suhashi.desilva@gmail.com>
2024-11-28 16:30:50 -0500

    script updates

--- WFSim/script.sh
+++ WFSim/script.sh
@@ -18,12 +18,17 @@ echo "Running script on single CPU core"
 
 folder="/blue/boucher/suhashidesilva/WFSim/WFSim/"
 output="/blue/boucher/suhashidesilva/WFSim/WFSim/output"
+logFile="/blue/boucher/suhashidesilva/WFSim/WFSim/output/execution_stats.log"
+
 
 lociList=(40 80 160 320)
 #individualSizeList=(50 100 200)
 #effectivePopulationRangeList=((50,100),(50,150),(50,250))
 #durationRangeList=((2,2),(4,4),(16,16))
 numReps=10
+
+echo "Loci,Iteration,Execution Time (s),Memory Used (MB)" > "$logFile"
+
 for loci in "${lociList[@]}"; do
 #for sampleSize in "${individualSizeList[@]}"; do
 #for effectivePopulation in "${effectivePopulationRangeList[@]}"; do
@@ -37,6 +42,7 @@ for loci in "${lociList[@]}"; do
     mutationRate=0.000000012     # Mutation rate
     rangeDuration="2 8"          # Space-separated range for Duration
 
+    start_time=$(date +%s)
     python main.py \
       --l "$loci" \
       --lNe $(echo $rangeNe | cut -d' ' -f1) \
@@ -50,11 +56,24 @@ for loci in "${lociList[@]}"; do
       --uD $(echo $rangeDuration | cut -d' ' -f2) \
       > /blue/boucher/suhashidesilva/WFSim/WFSim/output/"$outputFileName"
 
+        # End time and memory used
+    execution_time=$(cut -d' ' -f1 time_mem.log) # Execution time in seconds
+    memory_used=$(cut -d' ' -f2 time_mem.log)    # Memory used in KB
+
+    # Convert memory to MB
+    memory_used_mb=$(echo "scale=2; $memory_used / 1024" | bc)
+
+    # Write to the log file
+    echo "$loci,$i,$execution_time,$memory_used_mb" >> "$logFile"
+
+    # Sleep to prevent overloading
     sleep 1
   done
 done
 
+# Clean up temporary files
+rm -f time_mem.log
+
 
 date
-
 
